5e207dd04a414fed8497a8d33656d2fbec117a4d
DmitriyRF <dmitriy_r_f@mail.ru>
2017-07-13 01:28:19 +0300

    Gather some css files in one css file through libs.less file. File has import some file through (inline) option — include the source file in the output but do not process it

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp				=	require('gulp'),
 	less				=	require('gulp-less'),
+	path 				=	require('path'),
 	browserSync			=	require('browser-sync'),
 	concat				=	require('gulp-concat'),//объединение всех файлов в одни
 	uglify				=	require('gulp-uglifyjs');//сжатие файлов
@@ -8,13 +9,20 @@ var gulp				=	require('gulp'),
 //	--save-dev сохранение пакета и версии в папку или packaje.json
 
 gulp.task('less', function(){
-	return gulp.src('app/less/**/*.less')
-	.pipe(less())
+	return gulp
+	.src('app/less/**/*.less')
+	.pipe(less({ 
+		paths: [ path.join(__dirname)]  
+	})
+// 	.on('error', function(err) {
+//	      console.log(err);
+//	})
+    )
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch',['browserSync', 'less'], function(){
+gulp.task('watch',['browserSync', 'less', 'scripts'], function(){
 	gulp.watch('app/less/**/*.less', [less]);
 	gulp.watch('app/*.html', browserSync.reload);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
@@ -30,7 +38,8 @@ gulp.task('browserSync', function(){
 });
 
 gulp.task('scripts', function(){
-	return gulp.src([
+	return gulp
+	.src([
 			'app/assets/jquery/dist/jquery.min.js',
 			'app/assets/magnific-popup/dist/jquery.magnific-popup.min.js',
 			'app/assets/bootstrap/dist/js/bootstrap.min.js'
